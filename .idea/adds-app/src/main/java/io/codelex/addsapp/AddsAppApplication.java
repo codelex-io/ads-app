6a5b60450078a2709aab5777317d4436aebc4167
dneimanis <davis.neimanis@gmail.com>
2019-04-01 18:43:07 +0300

    initial commit

---
+++ .idea/adds-app/src/main/java/io/codelex/addsapp/AddsAppApplication.java
@@ -0,0 +1,13 @@
+package io.codelex.addsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddsAppApplication.class, args);
+	}
+
+}
