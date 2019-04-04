8c3137b929016375e4689c74c59826ed40e965df
dneimanis <davis.neimanis@gmail.com>
2019-04-04 19:31:41 +0300

    parsing done tests done

--- src/main/java/io/codelex/addsapp/AddsApplication.java
+++ src/main/java/io/codelex/addsapp/AddsApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdsApplication {
+public class AddsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdsApplication.class, args);
+        SpringApplication.run(AddsApplication.class, args);
     }
 }
