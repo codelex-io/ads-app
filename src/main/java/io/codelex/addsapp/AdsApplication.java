44e1b69d00faf070d7e76557013d9ef8834c0d74
dneimanis <davis.neimanis@gmail.com>
2019-04-04 19:43:52 +0300

    merged

--- src/main/java/io/codelex/addsapp/AdsApplication.java
+++ src/main/java/io/codelex/addsapp/AdsApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AddsAppApplication {
+public class AdsApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AddsAppApplication.class, args);
+        SpringApplication.run(AdsApplication.class, args);
     }
 
 }
